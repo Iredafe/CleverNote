a86ea88af6aabbab8f32e380b785944bc0c16ec2
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-07-21 21:05:42 -0700

    finish cleverNote application

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import AssignmentIcon from '@material-ui/icons/Assignment';
 
 function Header(){
 
 return <header>
-<h1>CleverNote</h1>
+<h1><AssignmentIcon /> CleverNote</h1>
 </header>
 
 }
